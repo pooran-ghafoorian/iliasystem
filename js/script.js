00929813bc22c8237ba4fbcc0aa8e50b8d16f634
puran <toosi@gmail.com>
2026-09-06 19:28:20 +0330

    first version

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// function  fact(x){
+// var a=1
+// for(var i=1;i<=x;i++){
+// a=a*i;
+// }
+// return a
+// }
+// var num = prompt("عدد را وارد کنید")
+// console.log(fact(num));
+function a(){
+    var box= document.getElementById("box");
+    box.classList.add("box");
+
+}
+// element.classList.add("show");
+
